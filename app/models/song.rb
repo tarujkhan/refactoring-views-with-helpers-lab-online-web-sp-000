f764ef9218aac0cff1dfbc8bef877e362f6dc46f
taroj khan <tarujkhan@yahoo.com>
2021-01-21 16:55:51 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,11 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    song = Song.new
+    song.save
   end
 
   def artist_name=(name)
+    song = Song.create(title:, artist_name:)
   end
 end
