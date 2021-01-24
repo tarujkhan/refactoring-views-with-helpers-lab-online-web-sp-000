bb92af960a871154a791a76aa6108722c13f4dfd
taroj khan <tarujkhan@yahoo.com>
2021-01-24 19:23:54 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,10 @@
 module ArtistsHelper
+  def display_artist=(name)
+    if song.artist
+      erb :'views/songs/show.html.erb'
+    else
+      erb :'views/songs/edit.html.erb'
+      flash[:message] = "Add Artist"
+end
+end
 end
